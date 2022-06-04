6dda24dc1e01b72c415584bdaf770d9c4ad34a31
ztgarry <as.tkachuk@gmail.com>
2022-06-04 19:21:22 +0300

    addnewlines

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Sasha!");
+Console.WriteLine("Hello, Masha!");
+Console.WriteLine("Hello, Dasha!");
+Console.WriteLine("Hello, Pasha!");
